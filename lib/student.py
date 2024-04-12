5c9b211ad947dd601733a961a340a5ba4a09fd46
Sylviachelimo <chelimosylvia280@gmail.com>
2024-04-12 20:53:59 +0300

    Completed

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,27 @@
 class Student:
-    pass
+    '''Class representing a Student'''
+
+    def hello(self):
+        '''Prints out a greeting message'''
+        print("Hey there! I'm so excited to learn stuff.")
+
+    def raise_hand(self):
+        '''Prints out a message indicating raising hand'''
+        print("Pick me!")
+
 
 class ChattyStudent(Student):
-    pass
+    '''Class representing a Chatty Student'''
+
+    def hello(self):
+        '''Augments the hello method to be more chatty'''
+        super().hello()
+        print("How are you doing today? I'm okay, but I'm kind of tired. "
+              "Did you watch The Walking Dead last night? You didn't?! "
+              "Oh man, it was so crazy! What, you don't want any spoilers? "
+              "Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        '''Overrides raise_hand to print "Pick me!" ten times'''
+        for _ in range(10):
+            super().raise_hand()
